9875b3dfeac6ebab0822d60ed5552582f003d266
innovatorCL <mycareer_CL@163.com>
2018-03-26 23:41:20 +0800

    手动添加Window

--- app/src/main/java/com/cn21/innovator/windowtest/WindowTestActivity.java
+++ app/src/main/java/com/cn21/innovator/windowtest/WindowTestActivity.java
@@ -2,12 +2,17 @@ package com.cn21.innovator.windowtest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class WindowTestActivity extends Activity {
+public class WindowTestActivity extends Activity implements OnTouchListener{
 
   private static final String TAG = "TestActivity";
 
@@ -30,7 +35,57 @@ public class WindowTestActivity extends Activity {
   }
 
   public void onButtonClick(View v){
-
+    if(v == mCreateWindowButton){
+      mFloatingButton = new Button(this);
+      mFloatingButton.setText("click me");
+      mLayoutParams = new WindowManager.LayoutParams(
+              WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, 0, 0,
+              PixelFormat.TRANSPARENT);
+      mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+              | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+              | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+      mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+      mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+      mLayoutParams.x = 100;
+      mLayoutParams.y = 300;
+      mFloatingButton.setOnTouchListener(this);
+      mWindowManager.addView(mFloatingButton, mLayoutParams);
+    }
   }
 
+  @Override
+  public boolean onTouch(View v, MotionEvent event) {
+    int rawX = (int) event.getRawX();
+    int rawY = (int) event.getRawY();
+    switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN: {
+        break;
+      }
+      case MotionEvent.ACTION_MOVE: {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        mLayoutParams.x = rawX;
+        mLayoutParams.y = rawY;
+        mWindowManager.updateViewLayout(mFloatingButton, mLayoutParams);
+        break;
+      }
+      case MotionEvent.ACTION_UP: {
+        break;
+      }
+      default:
+        break;
+    }
+
+    return false;
+  }
+
+  @Override
+  protected void onDestroy() {
+    try {
+      mWindowManager.removeView(mFloatingButton);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    }
+    super.onDestroy();
+  }
 }
